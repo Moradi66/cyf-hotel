ad3593667fe143163c818a896e8b95a9e75a2244
Moradi66 <mohsen000069@gmail.com>
2018-02-04 12:34:22 +0000

    doung post

--- index.js
+++ index.js
@@ -1,121 +1,110 @@
 const SERVER_PORT = process.env.PORT || 8080;
-
 const express = require("express");
 const exphbs = require("express-handlebars");
 const bodyparser = require("body-parser");
 const fs = require("fs");
-
 const apiRouter = require("./api");
-
 const app = express();
 const router = express.Router();
 const invoicesPath = __dirname + "/public/data/invoices.json";
 const customersPath = __dirname + "/public/data/customers.json";
-
-
-app.engine(
-  "hbs",
-  exphbs({
+app.engine("hbs", exphbs({
     defaultLayout: "main",
     extname: "hbs"
-  })
-);
-app.set("view engine", "hbs");
+}));
 
+app.set("view engine", "hbs");
 app.use(express.static("public"));
 app.use(express.static("assets"));
-
 app.use("/api", apiRouter);
 
 // handle HTTP POST requests
 app.use(bodyparser.json());
-
 app.get("/", function(req, res, next) {
-  res.render("home");
+    res.render("home");
 });
 
 //Render customers page
 app.get('/customers/', (req, res) => {
-  const callbackFunction = (error, file) => {
-    // we call .toString() to turn the file buffer to a String
-    const fileData = file.toString();
-    // we use JSON.parse to get an object out the String
-    const parsedFile = JSON.parse(fileData);
-    // send the json to the Template to render
-    res.render('customers', { 
-      objJson: parsedFile
-    });
-  };
-  fs.readFile(customersPath, callbackFunction);
+    const callbackFunction = (error, file) => {
+        // we call .toString() to turn the file buffer to a String
+        const fileData = file.toString();
+        // we use JSON.parse to get an object out the String
+        const parsedFile = JSON.parse(fileData);
+        // send the json to the Template to render
+        res.render('customers', {
+            objJson: parsedFile
+        });
+    };
+    fs.readFile(customersPath, callbackFunction);
 });
 
 //Render customers id page
-app.get('/customers/:id', (req,res,next) => {
-	fs.readFile(customersPath, (error,file) => {
-		const fileData = file.toString();
-    	const parsedFile = JSON.parse(fileData);
-    	res.render('customers', {objJson: parsedFile.filter(customer => customer.id === parseInt(req.params.id)) });
-	});
+app.get('/customers/:id', (req, res, next) => {
+    fs.readFile(customersPath, (error, file) => {
+        const fileData = file.toString();
+        const parsedFile = JSON.parse(fileData);
+        res.render('customers', {
+            objJson: parsedFile.filter(customer => customer.id === parseInt(req.params.id))
+        });
+    });
 });
-
-// Shoter code (customers + customers id pages)
-// app.get('/customers/:id?', (req,res,next) => {
-// 	fs.readFile(customersPath, (error,file) => {
-// 		const fileData = file.toString();
-//     	const parsedFile = JSON.parse(fileData);
-//     	if (req.params.id) {
-//     		res.render('customers', {objJson: parsedFile.filter(customer => customer.id === parseInt(req.params.id)) });
-//     	} else {
-//     		res.render('customers', {objJson: parsedFile});
-//     	};
-// 	});
-// });
 
 // Render invoice page
 app.get('/invoices/', (req, res) => {
-  const callbackFunction = (error, file) => {
-
-    // we call .toString() to turn the file buffer to a String
-    const fileData = file.toString();
-    // we use JSON.parse to get an object out the String
-    const parsedFile = JSON.parse(fileData);
-    // send the json to the Template to render
-    res.render('invoices', { 
-      objJson: parsedFile
-    });
-  };
-  fs.readFile(invoicesPath, callbackFunction);
+    const callbackFunction = (error, file) => {
+        // we call .toString() to turn the file buffer to a String
+        const fileData = file.toString();
+        // we use JSON.parse to get an object out the String
+        const parsedFile = JSON.parse(fileData);
+        // send the json to the Template to render
+        res.render('invoices', {
+            objJson: parsedFile
+        });
+    };
+    fs.readFile(invoicesPath, callbackFunction);
 });
 
 //Render invoices id page
 app.get('/invoices/:id', (req, res, next) => {
-	fs.readFile(invoicesPath, (error, file) => {
-		const fileData = file.toString();
-    	const parsedFile = JSON.parse(fileData);
-    	res.render('invoices', {objJson: parsedFile.filter(invoice => invoice.id === parseInt(req.params.id)) });
-	});
+    fs.readFile(invoicesPath, (error, file) => {
+        const fileData = file.toString();
+        const parsedFile = JSON.parse(fileData);
+        res.render('invoices', {
+            objJson: parsedFile.filter(invoice => invoice.id === parseInt(req.params.id))
+        });
+    });
 });
 
-      // PUT CODE FOR RENDERING INVOICE ID PAGE HERE
-      
-
-      // var button = select('#submit-btn');
-      // button.mousePressed(submitInvoince);
-
-      // function submitInvoince(){
-      //   var id = select('#id-submit').value();
-      //   var reservationId = select('#reservation-id-submit').value();
-      //   var total = select('#total-submit').value();
-      //   var surcharges = select('#surcharges-submit').value();
-      //   var invoiceDateTime = select('#invoice-date-time-submit').value();
-      //   var paid = select('#paid-submit').value();
-      //   console.log(id)
-      // }
-
-
-      
+app.use(bodyparser());
+// PUT CODE FOR RENDERING INVOICE ID PAGE HERE
+app.post("/submit-form", function(req, res) {
+    fs.readFile(__dirname + '/public/data/invoices.json', function(error, file) {
+        var parsedFile = JSON.parse(file);
+        // console.log(parsedFile)
+      fs.writeFile(__dirname +'/public/data/invoices.json',
+       JSON.stringify(parsedFile, null, 2),
+       function(error) {
+        if (req.body.id){
+                  
+        }else{
+            console.log('write down the id');
+        }
+      });
+    });
+    res.end(JSON.stringify(req.body))
+    // var arr = req.fields
+    // console.log(arr)
+    //    var data = JSON.stringify(arr);
+    // fs.readFile(__dirname + 'public/data/invoices.json', function (error, file) {
+    //     var parsedFile = JSON.parse(file);
+    //    parsedFile = [arr]
+    //     fs.writeFile('public/data/invoices.json',  JSON.stringify(parsedFile), function (error) {        
+    //     });
+    // });
+});
 
 
 app.listen(SERVER_PORT, () => {
-  console.info(`Server started at http://localhost:${SERVER_PORT}`);
+    console.info(`Server started at http://localhost:${SERVER_PORT}`);
 });
